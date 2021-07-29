9083a2aec87d04a2943460be68c88129751341ea
Luis Chamorro <ing.lfchamorro@gmail.com>
2021-07-28 21:16:36 -0500

    Se obtienen datos de un POST

--- controllers/user.js
+++ controllers/user.js
@@ -8,8 +8,14 @@
     };
 
     const usersPost = (req, res = response) => {
+
+        const { nombre, edad, id } = req.body;
+
         res.json({
-            message: 'POST API - controllador'
+            message: 'POST API - controllador',
+            nombre,
+            edad,
+            id
         });
     };
 
