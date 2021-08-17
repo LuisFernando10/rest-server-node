ed7c875f5c1535cf13144262f665b6f29a964e10
Luis Fernando <desarrollo@fcomunicaciones.com>
2021-08-17 16:18:51 -0500

    Se realizan validaciones para controlar el  login del usuario

--- controllers/login.js
+++ controllers/login.js
@@ -1,9 +1,49 @@
+    
     const { response } = require('express');
+    const bcryptjs = require('bcryptjs');
 
-    const logIn = ( req, res = response ) => {
-        res.json({
-            message: 'Controlador Login'
-        });
+    const User = require('../models/user');
+
+    const logIn = async( req, res = response ) => {
+
+        const { correo, password } = req.body;
+
+        try {
+            
+            // Verificar si el email existe
+            const user = await User.findOne({ correo });
+            if ( !user ){
+                return res.status(400).json({
+                    message: 'Usuario/Contraseña no son correctos - Correo'
+                });
+            }
+
+            // Verificar si el usuario está activo
+            if ( !user.estado ){
+                return res.status(400).json({
+                    message: 'No es un estado válido para el usuario - Estado'
+                });
+            }
+
+            //Verificar contraseña
+            const valid_password = bcryptjs.compareSync( password, user.password );
+            if ( !valid_password ){
+                return res.status(400).json({
+                    message: 'Contraseña incorrecta. - Contraseña'
+                });
+            }
+
+            // Generar el JWT
+
+
+            res.json({
+                message: 'Controlador Login'
+            });   
+        } catch (error) {
+            res.status(500).json({
+                message: 'Comuníquese con el administrador.'
+            })
+        }
     };
 
     module.exports = {
